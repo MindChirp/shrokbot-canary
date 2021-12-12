f2c0b1f690b4b98471e1cf3f8f953571098970e5
MindChirp <frikk44@gmail.com>
2021-12-13 00:15:00 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -37,40 +37,6 @@ module.exports = {
 
         //Check if there are videos in queue
         
-
-        //Get the queue database
-        try {
-            var db = await dbHandler.get(path.join(path.dirname(__dirname), "database", "queue"), "queue" + message.guild.id);
-        } catch (error) {
-            try {
-                //Does not exist
-                var db = await dbHandler.create("queue" + message.guild.id, path.join(path.dirname(__dirname), "database", "queue"));
-                await db.CREATE("username", "string");
-                await db.CREATE("video", "object");
-                await db.CREATE("place", "number");
-            } catch (error) {
-                message.channel.send("Sorry, your request couldn't be fulfilled.");
-                return;
-            }
-        }
-
-        try {
-            var nowDb = await dbHandler.get(path.join(path.dirname(__dirname), "database", "playing"), "playing" + message.guild.id);
-        } catch (error) {
-            //Does not exist
-            try {
-                var nowDb = await dbHandler.create("playing" + message.guild.id, path.join(path.dirname(__dirname), "database", "playing"));
-                await nowDb.CREATE("username", "string");
-                await nowDb.CREATE("video", "object");
-                await nowDb.CREATE("order", "number");
-            } catch (error) {
-                console.log(error);
-                message.channel.send("Could not load song.");
-                return;
-            }
-        }
-
-
         await db.INSERT({username: message.author.username, video: video, place: 0});
 
         //Check for queue
@@ -107,10 +73,6 @@ module.exports = {
 
         async function playVideo(video) {
             if(video) {
-                //Add this video object to the now playing database
-                replaceNowPlayingData(video);
-
-
                 async function playAudio() {
                     var stream = ytdl(video.url, {filter:'audioonly'});
                     connection.play(stream, {seek: 0, volume: 1})
@@ -147,75 +109,6 @@ module.exports = {
             } else {
                 message.channel.send("No videos were found.");
             }
-        }
-
-
-        async function replaceNowPlayingData(video) {
-            //Replace the currently playing video with a new one
-            //Remove the old video from queue list
-            
-            //Get the currently playing video
-            var nowPlaying = await nowDb.SELECT("order", 0);
-            if(!(nowPlaying.length < 1)) {
-                try {
-                    await nowDb.WIPE();
-                } catch (error) {
-                    console.log(error);
-                }
-
-                try {
-                    await nowDb.INSERT({username: message.author.username, video: video, order: 0});
-                } catch (error) {
-                    console.log(error);
-                }
-            } else {
-                try {
-                    await nowDb.INSERT({username: message.author.username, video: video, order: 0});
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-
-        }
-
-        async function playNextVideo(playedVideo) {
-            //Play the next video, and kill the old one
-            //Fetch the queue, is there something more?
-            try {
-                var queue = await db.SELECT("*");
-            } catch (error) {
-                //No queue
-                setTimeout(async ()=>{
-                    await vc.leave();
-                }, 1000);
-                return;
-            }
-            if(queue[0].values.length == 1 && queue[1].values[0].value == playedVideo || queue[0].values.length == 1 && !playedVideo) {
-                //Do not continue
-                //Clear the now playing
-                await nowDb.WIPE();
-                await db.WIPE();
-                var vc = message.member.voice.channel;
-                setTimeout(async ()=>{
-                    await vc.leave();
-                }, 1000);
-
-                return;
-            } else {
-                //There is a queue, play the next one
-                var now = await nowDb.SELECT("order", 0);
-                try {
-                    await db.DELETE("video", now[1]) //Delete the row with the currently playing video
-                    //Get the next video
-                    var video = await db.SELECT("*");
-                    playVideo(video[1].values[0].value);
-
-                } catch (error) {
-                    console.log(error);
-                }
-
-            }
-
         }
 
     }
