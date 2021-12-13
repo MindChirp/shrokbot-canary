5c375e3ff5e8310f88ed93861f9b14478d885957
MindChirp <frikk44@gmail.com>
2021-12-13 13:57:19 +0100

    Redesigned queue

--- commands/queue.js
+++ commands/queue.js
@@ -35,8 +35,22 @@ module.exports = {
         .setTitle('Your queue')
 
         var x;
-        for(x of list) {
-            queueEmbed.addField(x.video.title, x.video.timestamp + " | Requested by " + x.user.username);
+        for(let i = 0; i < list.length; i++) {
+            //Treat the video title
+            var title = list[i].video.title;
+            
+            function trunc(txt, length) {
+                var str = txt;
+                if(str.length > length) {
+                    return txt.slice(0,length) + "...";
+                } else {
+                    return txt;
+                }
+            }
+
+            var truncated = trunc(title,40);
+
+            queueEmbed.addField("**`" + i+1 + ".`**", truncated + " " + list[i].video.timestamp + " | Requested by " + list[i].user.username);
         }
         
         message.channel.send(queueEmbed);
