797f81911ecb524a9475e7df81fba8e8e626ec69
MindChirp <frikk44@gmail.com>
2022-11-29 15:17:49 +0100

    Reset everything

--- commands/fuckoff.js
+++
@@ -1,9 +0,0 @@
-const {prefix, token} = require('../config.json');
-
-module.exports = {
-    name: "fuckoff",
-    description: "Plays music from YouTube",
-    async execute(message, args) {
-        message.channel.send("This function is under development!");
-    }
-}
