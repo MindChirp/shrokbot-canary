9811d4c97d1e943b5dfb1059588ec76424ac10dd
MindChirp <frikk44@gmail.com>
2022-12-05 14:40:51 +0100

    Create siwallah.js

---
+++ commands/siwallah.js
@@ -0,0 +1,22 @@
+const {SlashCommandBuilder} = require('discord.js');
+const {GuildVoiceClasses} = require('../play/streams');
+
+/**
+ * THIS COMMAND IS EXCLUSIVELY FOR A SERVER OF A FRIEND OF MINE
+ * -Frikk Balder
+ */
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('siwallah')
+    .setDescription('Sier wallah')
+    .setDMPermission(false),
+
+  async execute(interaction, client) {
+    if (interaction.guild.id != '263300337320853505') {
+      interaction.reply('This command is disabled for this server');
+      return;
+    }
+    interaction.reply(interaction.member.toString() + ' wallah');
+  },
+};
